ad5f6ae8f4c718a239e2a6bb65ef76594e071d84
Alex Standiford <tstandiford@gmail.com>
2017-05-16 15:25:00 -0400

    Customized the timepicker to store the value in the customizer

--- assets/timepicker.js
+++ assets/timepicker.js
@@ -4,13 +4,21 @@ jQuery(document).ready(function($){
   });
   $('#sub-accordion-section-esh_store_hours').find('.button-secondary').on('click',function(e){
     e.preventDefault();
-    var eshOpenField = $(this).next();
-    var eshClosedField = $(eshOpenField).next();
-    var eshClosedCheckbox = $(eshClosedField).next();
+    var eshOpenField = $(this).next().find('input');
+    var eshClosedField = $(eshOpenField).next().find('input');
+    var eshClosedCheckbox = $(eshClosedField).next().find('input');
 
-    eshOpenField.find('input').val("");
-    eshClosedField.find('input').val("");
-    eshClosedCheckbox.find('input').prop('checked',false);
-
+    eshOpenField.val("");
+    wp.customize(eshOpenField.attr('id'),function(obj){
+      obj.set("");
+    });
+    eshClosedField.val("");
+    wp.customize(eshClosedField.attr('id'),function(obj){
+      obj.set("");
+    });
+    eshClosedCheckbox.prop('checked',false);
+    wp.customize(eshClosedCheckbox.attr('data-customize-setting-link'),function(obj){
+      obj.set("");
+    })
   })
 });
