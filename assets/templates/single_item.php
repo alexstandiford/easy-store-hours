4fc7e4868534e9492cb9aa0d18e91b934c2415ef
Alex Standiford <tstandiford@gmail.com>
2017-05-18 16:13:30 -0400

    Rewrote single item template to support schema markup

--- assets/templates/single_item.php
+++ assets/templates/single_item.php
@@ -1,3 +1,14 @@
-<li <?php echo $store_hours->singleClass(); ?>>
-  <span class="day"><?php echo $store_hours->currentDay->name; ?></span>: <span class="hours"><?php echo $store_hours->getHours(); ?></span>
+<li itemprop="openingHoursSpecification" itemscope itemtype="http://schema.org/OpeningHoursSpecification" <?php echo $store_hours->singleClass(); ?>>
+  <span itemprop="dayOfWeek" class="day">
+    <?php echo $store_hours->currentDay->name; ?>
+  </span>:
+  <span class="hours">
+    <time itemprop="opens" datetime="<?php echo $store_hours->currentDay->getTimestamp('open'); ?>">
+      <?php echo $store_hours->currentDay->getTime('open'); ?>
+    </time>
+    <?php echo $store_hours->hourSeparator; ?>
+    <time itemprop="closes" datetime="<?php echo $store_hours->currentDay->getTimestamp('closed'); ?>">
+      <?php echo $store_hours->currentDay->getTime('closed'); ?>
+    </time>
+  </span>
 </li>
