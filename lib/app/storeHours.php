1f5f50978d57537a205ca9664353fce98a4aa9d5
Alex Standiford <tstandiford@gmail.com>
2017-05-18 15:36:21 -0400

    Added Widget and Shortcode callback functions.

--- lib/app/storeHours.php
+++ lib/app/storeHours.php
@@ -17,15 +17,17 @@ class storeHours{
     $this->beforeHour = $this->validateValue($args['before_hour'], apply_filters('esh_before_hour', ''));
     $this->afterHour = $this->validateValue($args['after_hour'], apply_filters('esh_after_hour', ''));
     $this->closedText = $this->validateValue($args['closed_text'], apply_filters('esh_closed_text', 'Closed'));
+    $this->ranAsShortcode = $args['ran_as_shortcode'] ? true : false;
+    $this->ranAsWidget = $args['ran_as_widget'] ? true : false;
   }
 
   private static function validateValue($value_if_true, $value_if_false){
     return is_string($value_if_true) ? $value_if_true : $value_if_false;
   }
 
-  public static function get(){
+  public static function get($args){
     $result = '';
-    $store_hours = new self();
+    $store_hours = new self($args);
     ob_start();
     include(template::getFile('header'));
     foreach($store_hours->query->days as $day){
@@ -34,11 +36,18 @@ class storeHours{
     }
     include(template::getFile('footer'));
     $result = ob_get_clean();
+
     return $result;
   }
 
-  public static function getShortcode(){
-    return self::get();
+  public static function getShortcode($args = array()){
+    $args['ran_as_shortcode'] = true;
+    return self::get($args);
+  }
+
+  public static function getWidget($args = array()){
+    $args['ran_as_widget'] = true;
+    return self::get($args);
   }
 
   public function getHours($day = false, $separator = false, $before = false, $after = false, $closed_text = false){
@@ -82,6 +91,7 @@ class storeHours{
     $default_classes = apply_filters('esh_wrapper_classes', array(
       'store-hours-wrapper'     => true,
       'store-hours-shortcode-item' => $this->ranAsShortcode,
+      'store-hours-widget-item' => $this->ranAsWidget,
     ));
     $args = array_merge($default_classes, $extra_classes);
     $classes = new cssClassSet($args);
